961540cdc4173f57cf8dfdbb25568227dea78513
罗时迁 <luoshiqian@ymatou.com>
2017-03-23 15:05:38 +0800

    first commit

---
+++ facade-interface/src/main/java/com/ymatou/mq/rabbit/dispatcher/facade/DemoFacade.java
@@ -0,0 +1,15 @@
+/*
+ *
+ *  (C) Copyright 2017 Ymatou (http://www.ymatou.com/).
+ *  All rights reserved.
+ *
+ */
+
+package com.ymatou.mq.rabbit.dispatcher.facade;
+/**
+ * @author luoshiqian 2016/8/31 14:12
+ */
+public interface DemoFacade {
+
+    String sayHello(String name);
+}
