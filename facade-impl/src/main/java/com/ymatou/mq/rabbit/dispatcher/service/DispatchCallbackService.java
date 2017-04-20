6c7a959f6835956b8590e78d6f8dcef3a89f4d28
tuuutuuu <tuwenjie@ymt.corp>
2017-04-20 14:14:43 +0800

    code review and fix

--- facade-impl/src/main/java/com/ymatou/mq/rabbit/dispatcher/service/DispatchCallbackService.java
+++ facade-impl/src/main/java/com/ymatou/mq/rabbit/dispatcher/service/DispatchCallbackService.java
@@ -102,6 +102,7 @@ public class DispatchCallbackService implements HttpInvokeResultService {
         try {
             //更新分发明细状态
             CallbackResult callbackResult = this.buildCallbackResult(message,callbackConfig,message.getResponse());
+            //FIXME：应该插指令
             messageService.updateDispatchDetail(callbackResult);
         } catch (Exception e) {
             logger.error("onInvokeSuccess proccess error.",e);
